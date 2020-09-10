6d807b29546070a5ad29d3e7a9ce562343b4268c
ZSQ <zsq19991106@aliyun.com>
2020-09-10 20:12:02 +0800

    历史记录框架

---
+++ app/src/main/java/com/java/zhushuqi/HistoryActivity.java
@@ -0,0 +1,13 @@
+package com.java.zhushuqi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
