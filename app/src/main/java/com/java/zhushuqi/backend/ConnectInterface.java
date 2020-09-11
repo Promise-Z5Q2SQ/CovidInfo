f1e44d82125b04d1c5ccf3bace4d6c75231bb830
sj-zhang18 <1754654240@qq.com>
2020-09-11 12:00:22 +0800

    f
    f

--- app/src/main/java/com/java/zhushuqi/backend/ConnectInterface.java
+++ app/src/main/java/com/java/zhushuqi/backend/ConnectInterface.java
@@ -1,10 +1,18 @@
 package com.java.zhushuqi.backend;
 
+import android.content.Context;
 import android.os.Build;
+import android.os.FileUtils;
 import android.provider.ContactsContract;
 
 import androidx.annotation.RequiresApi;
 
+import com.java.zhushuqi.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.security.Key;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -13,6 +21,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.reactivestreams.*;
 
 public class ConnectInterface {
@@ -262,6 +272,89 @@ public class ConnectInterface {
             @Override
             public Publisher<Scholar> apply(List<Scholar> entities) {
                 return Flowable.fromIterable(entities);//fixme 如果运行了这一句代表网络出现问题没有正常返回
+            }
+        }).toList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Single<List<Keyword>> GetKeyword(int c, Context context) {
+        return Flowable.fromCallable(new Callable<List<Keyword>>() {
+            @Override
+            public List<Keyword> call() throws Exception {
+                try {
+                    List<Keyword> ls = new ArrayList<Keyword>();
+                    String msg = "";
+                    String content = "";
+                    BufferedReader reader = null;
+                    if(c == 0){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.keyword0)));
+                    }
+                    else if(c == 1){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.keyword1)));
+                    }
+                    else if(c == 2){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.keyword2)));
+                    }
+                    else{
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.keyword3)));
+                    }
+                    while((msg = reader.readLine()).length() > 0){
+                        content = content += msg;
+                    }
+                    JSONArray arr = new JSONArray(content);
+                    for(int i = 0; i < 4; i++){
+                        JSONArray ar = arr.optJSONArray(i);
+                        Keyword ky = new Keyword(ar.optString(0), ar.optInt(1));
+                        ls.add(ky);
+                    }
+                    return ls;
+                } catch (Exception e) {
+                    return new ArrayList<Keyword>();
+                }
+            }
+        }).flatMap(new Function<List<Keyword>, Publisher<Keyword>>() {
+            @Override
+            public Publisher<Keyword> apply(List<Keyword> entities) {
+                return Flowable.fromIterable(entities);//fixme 如果运行了这一句代表网络出现问题没有正常返回
+            }
+        }).toList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public static Single<List<News>> GetCluster(int c, Context context) {
+        return Flowable.fromCallable(new Callable<List<News>>() {
+            @Override
+            public List<News> call() throws Exception {
+                try {
+                    String msg = "";
+                    String content = "";
+                    BufferedReader reader = null;
+                    if(c == 0){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.category0)));
+                    }
+                    else if(c == 1){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.category1)));
+                    }
+                    else if(c == 2){
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.category2)));
+                    }
+                    else{
+                        reader = new BufferedReader(new InputStreamReader(context.getResources().openRawResource(R.raw.category3)));
+                    }
+                    while((msg = reader.readLine()).length() > 0){
+                        content = content += msg;
+                    }
+                    JSONArray arr = new JSONArray(content);
+                    Server.server.ReadCluster(arr, Server.server.Cluster[c]);
+                    System.out.println(Server.server.Cluster.toString());
+                    return Server.server.Cluster[c];
+                } catch (Exception e) {
+                    return new ArrayList<News>();
+                }
+            }
+        }).flatMap(new Function<List<News>, Publisher<News>>() {
+            @Override
+            public Publisher<News> apply(List<News> Newses) {
+                if (Newses.size() > 0) return Flowable.fromIterable(Newses);
+                return Flowable.fromIterable(Server.server.NewsInShow);//fixme 如果运行了这一句代表网络出现问题没有正常返回
             }
         }).toList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
